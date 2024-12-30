f3b79ad199c25b3afb651ca968baf5e62f0a2892
José Francisco Veiga <veigajosefrancisco@gmail.com>
2024-12-30 16:41:35 +0000

    Fix MyPowerUp typos

--- tp3/objects/MyReader.js
+++ tp3/objects/MyReader.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { MyBalloon } from "./MyBalloon.js";
 import { MyObstacle } from "./MyObstacle.js";
-import { MyPowerup } from "./MyPowerup.js";
+import { MyPowerUp } from "./MyPowerUp.js";
 import { MyRoute } from "./MyRoute.js";
 import { MyTrack } from "./MyTrack.js";
 
