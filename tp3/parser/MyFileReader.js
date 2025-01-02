62771caddf40007f567e3e0f476b77f27452f19d
José Francisco Veiga <veigajosefrancisco@gmail.com>
2025-01-02 20:47:54 +0000

    Continue picking

--- tp3/parser/MyFileReader.js
+++ tp3/parser/MyFileReader.js
@@ -20,12 +20,11 @@ class MyFileReader {
 	   constructs the object
 	*/
 	constructor(onSceneLoadedCallback) {
-		this.errorMessage = null;
 		this.onSceneLoadedCallback = onSceneLoadedCallback;
 	}
 
-	open(jsonfile) {
-		fetch(jsonfile)
+	async open(jsonfile) {
+		return fetch(jsonfile)
 			.then((res) => {
 				if (!res.ok) {
 					throw new Error(`HTTP error! Status: ${res.status}`);
@@ -35,10 +34,8 @@ class MyFileReader {
 			.then((data) => {
 				this.onSceneLoadedCallback(data);
 			})
-			.catch((error) =>
-				console.error("Unable to fetch data:", error));
+			.catch((error) => console.error("Unable to fetch data:", error));
 	};
-
 }
 
 export { MyFileReader };
